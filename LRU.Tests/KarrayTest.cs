3c2d53f0b2f5b512bc527b258dba2fdafcd4a473
Gabor Benko <benkogabor@msn.com>
2020-10-27 21:51:52 +0100

    KArray

---
+++ LRU.Tests/KarrayTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnitTestsLRUKelementArray;
+
+namespace Tests
+{
+    [TestFixture]
+    public class KArrayTest
+    {
+        [Test]
+        public void expectedSol()
+        {
+            int[] A = new int[] { 1, 2, 1, 2, 3, 2, -3, -1, -2 };
+
+            var result = KarraySol.Sol(A);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void should_return_zero_when_not_paired()
+        {
+            int[] A = new int[] { 1, 2, 2, 3, 4, 5 };
+
+            var result = KarraySol.Sol(A);
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+    }
+}
---
+++ UnitTestsLRUKelementArray/KarraySol.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace UnitTestsLRUKelementArray
+{
+    public class KarraySol
+    {
+        public static int Sol(int[] A)
+        {
+            HashSet<int> opposites = new HashSet<int>();
+            int result = 0;
+
+            foreach(int item in A)
+            {
+                if(opposites.Contains(-1 * item))
+                {
+                    result = Math.Max(result, item < 0 ? item * -1 : item);
+                }
+                else
+                {
+                    opposites.Add(item);
+                }
+            }
+
+            return result;
+        }
+    }
+}
